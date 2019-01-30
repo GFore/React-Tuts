a9f63d108d57c3e8685821b200408a29239020a9
Greg Foreman <gfore@mindspring.com>
2019-01-30 00:20:24 -0500

    adds child component to test passing props

--- src/App.js
+++ src/App.js
@@ -4,27 +4,45 @@ import './App.css';
 
 class App extends Component {
   render() {
-    let word = "World!";
     return (
       <div className="App">
-        <h1>Hello {word}</h1>
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> 
-        </header>*/}
+        <h1>Hello ord</h1>
+        <ul>
+          <ListItem quantity="1">Bread</ListItem>
+          <ListItem quantity="6">Eggs</ListItem>
+          <ListItem quantity="2">Milk</ListItem>
+        </ul>
+
       </div>
     );
   }
 }
 
+class ListItem extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <li>
+        {this.props.quantity} x {this.props.children}
+      </li>
+    );
+  }
+}
+
 export default App;
+
+
+// <header className="App-header">
+// <img src={logo} className="App-logo" alt="logo" />
+// <p>
+//   Edit <code>src/App.js</code> and save to reload.
+// </p>
+// <a
+//   className="App-link"
+//   href="https://reactjs.org"
+//   target="_blank"
+//   rel="noopener noreferrer"
+// >
+//   Learn React
+// </a> 
+// </header>

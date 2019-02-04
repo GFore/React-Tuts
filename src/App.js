d80d06d5600c896d27a8dc80d9e63e75720f79a0
Greg Foreman <gfore@mindspring.com>
2019-02-03 23:36:22 -0500

    adds CSS styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
 import KanbanBoard from './KanbanBoard';
 
 let cardsList = [
@@ -59,20 +57,3 @@ export default App;
 //     );
 //   }
 // }
-
-
-
-// <header className="App-header">
-// <img src={logo} className="App-logo" alt="logo" />
-// <p>
-//   Edit <code>src/App.js</code> and save to reload.
-// </p>
-// <a
-//   className="App-link"
-//   href="https://reactjs.org"
-//   target="_blank"
-//   rel="noopener noreferrer"
-// >
-//   Learn React
-// </a> 
-// </header>

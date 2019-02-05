d3e58a7b26c04e7fb46fe1c7df108b88056742fb
Greg Foreman <gfore@mindspring.com>
2019-02-04 23:56:53 -0500

    adds toggleDetails method and activates the expand-collapse feature

--- src/Card.js
+++ src/Card.js
@@ -9,6 +9,10 @@ class Card extends Component {
         };
     }
 
+    toggleDetails() {
+        this.setState({showDetails: !this.state.showDetails});
+    }
+
     render() {
         let cardDetails;
         if (this.state.showDetails) {
@@ -18,14 +22,11 @@ class Card extends Component {
                     <CheckList cardId={this.props.id} tasks={this.props.tasks} />
                 </div>
             );
-        };
+        }
 
         return(
             <div className="card">
-                <div className="card__title" onClick={
-                        () => this.setState({showDetails: !this.state.showDetails})
-                    }
-                >
+                <div className={this.state.showDetails ? "card__title card__title--is-open" : "card__title"} onClick={this.toggleDetails.bind(this)}>
                     {this.props.title}
                 </div>
                 {cardDetails}

2ce96a43be5f493878c9c89c941f66cf9d908785
Greg Foreman <gfore@mindspring.com>
2019-02-24 11:45:05 -0500

    cleaning up the code and display

--- src/App.js
+++ src/App.js
@@ -4,22 +4,39 @@ import KanbanBoard from './KanbanBoard';
 let cardsList = [
   {
     id: 1,
+    status: "in-progress",
     title: "Read the Book",
     description: "I should read the **whole** book",
     color: '#BD8D31',
-    status: "in-progress",
     tasks: []
   },
   {
     id: 2,
+    status: "todo",
     title: "Write some code",
     description: "Code along with some samples in the book. The complete source can be found at [GitHub](https://github.com/pro-react)",
     color: '#3A7E28',
-    status: "todo",
     tasks: [
       {id: 1, name: "ContactList Example", done: true},
       {id: 2, name: "Kanban Example",done: false},
       {id: 3, name: "My own experiments", done: false}
+    ]
+  },
+  {
+    id: 3,
+    status: "todo",
+    title: "Learn more CSS Properties",
+    description: "These CSS properties are used in this project and I need to better understand what they do",
+    color: '#3A7E28',
+    tasks: [
+      {id: 1, name: "box-sizing: border-box", done: false},
+      {id: 2, name: "white-space: nowrap (or normal)",done: false},
+      {id: 3, name: "display: inline-block", done: false},
+      {id: 4, name: "vertical-align: top", done: false},
+      {id: 5, name: "overflow: auto", done: false},
+      {id: 6, name: "Selector: .list:not(:last-child):after", done: false},
+      {id: 7, name: "Selector: :before", done: false},
+      {id: 8, name: "Selector: :first-child", done: false},
     ]
   },
 ];
--- src/List.js
+++ src/List.js
@@ -3,7 +3,7 @@ import Card from './Card';
 
 class List extends Component {
     render() {
-        let cards = this.props.cards.map((card) => {
+        const cards = this.props.cards.map((card) => {
             return <Card id={card.id}
                          title={card.title}
                          description={card.description}
@@ -14,7 +14,7 @@ class List extends Component {
 
         return(
             <div className="list">
-                <h1>{this.props.title}</h1>
+                <h2>{this.props.title}</h2>
                 {cards}
             </div>
         );
